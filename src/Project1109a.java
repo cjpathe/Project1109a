962180407ff0f746294c549ff059b856767ccf1c
oracle <oracle@GBTC441R17RH.montgomerycollege.edu>
2016-11-09 19:14:06 -0500

    done

--- src/Project1109a.java
+++ src/Project1109a.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Project1109a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		for (int i=1; i<10; i++)
+			System.out.println("Hello World " + i + " !");
+		
+	    Scanner keyboard = new Scanner(System.in);
 
+	    int num1, num2, num3;
+
+	    System.out.print( "First temperature? " );
+	    num1 = keyboard.nextInt();
+
+	    System.out.print( "Second temperature? " );
+	    num2 = keyboard.nextInt();
+
+	    System.out.println("The max value is : " + Math.max(num1, num2));
 	}
 
 }
